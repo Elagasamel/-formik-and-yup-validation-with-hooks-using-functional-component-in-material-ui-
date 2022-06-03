e7a85483f510a982328555800be2dd8949748afd
Amel ELAGAS <lagas.amel@gmail.com>
2022-06-03 11:30:52 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import SignInOutContainer from './containers';
+function App() {
+  return (
+    <div className="App">
+     <SignInOutContainer/> 
+    </div>
+  );
+} 
+
+export default App;
